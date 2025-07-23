6e643a3cf66a382aa49ba43b1a6f673a5f630c22
Shankar-105 <shankarbhavani862@gmail.com>
2025-07-23 11:00:09 +0530

    Added The Tree Building Logic which constructs the Tree From the User Input + Renamaed The Files To Avoid Naming Conflicts

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main{
-    public static void handleLinkedLists(LinkedList head,Scanner sc){
+    public static void handleLinkedLists(MyLinkedList head,Scanner sc){
         System.out.println("Welcome to Linked List Operations---Firstly Construct a Linked List");
         System.out.println("Enter the values of each Node in the Linked List(Enter -1 to Stop)");
         while(true){
@@ -8,10 +8,10 @@ public class Main{
             if(value==-1){
                 break;
             }
-           head=LinkedList.createLinkedList(head,value);
+           head=MyLinkedList.createLinkedList(head,value);
         }
         System.out.println("Constructed Linked List...");
-        LinkedList.printLinkedList(head);
+        MyLinkedList.printLinkedList(head);
         System.out.println("Linked List Operations as follows");
             System.out.println("1. Insert at the End");
             System.out.println("2. Insert at the Beginning");
@@ -31,17 +31,17 @@ public class Main{
             case 1:
             System.out.println("Enter the Value to be Inserted at the end");
             int valEnd=sc.nextInt();
-            head=LinkedList.insertAtTail(head,valEnd);
+            head=MyLinkedList.insertAtTail(head,valEnd);
             System.out.println("Successfully appended:)");
             break;
             case 2:
             System.out.println("Enter the Value to be Inserted at the beginning");
             int valSt=sc.nextInt();
-            head=LinkedList.insertAtHead(head,valSt);
+            head=MyLinkedList.insertAtHead(head,valSt);
             System.out.println("Successfully pushed at the Beginning:)");
             break;
             case 8:
-           head=LinkedList.reverLinkedList(head);
+           head=MyLinkedList.reverLinkedList(head);
            System.out.println("Oops You Just Performed the Reverse Operation...The List gets Changed Entirely have a look at it...");
            break;
             case 3:
@@ -49,33 +49,33 @@ public class Main{
             int posK=sc.nextInt();
             System.err.println("Enter the Value");
             int valK=sc.nextInt();
-            head=LinkedList.insertAtK(head,posK,valK);
+            head=MyLinkedList.insertAtK(head,posK,valK);
             System.out.println("Pushed in Between Nodes "+(posK-1)+" and "+(posK));
             System.out.println("Wanna look at the List(Click 9)");
             break;
             case 9:
-            LinkedList.printLinkedList(head);
+            MyLinkedList.printLinkedList(head);
             break;
             case 10:
             return;
             case 4:
-            head=LinkedList.deleteHead(head);
+            head=MyLinkedList.deleteHead(head);
             System.out.println("Deleted The Head.\nWanna See the Change(Click 9)");
             break;
             case 5:
-            head=LinkedList.deleteTail(head);
+            head=MyLinkedList.deleteTail(head);
             System.out.println("Removed:)");
             break;
             case 6:
             System.out.println("Enter the position K");
             int delposK=sc.nextInt();
-            head=LinkedList.deleteAtK(head,delposK);
+            head=MyLinkedList.deleteAtK(head,delposK);
             System.out.println("The Node at position "+delposK+" is removed<<\nHave A Look at the List for Better Experiance.");
             break;
             case 7:
             System.out.println("Enter the Value of the Node to be deleted");
             int nodeVal=sc.nextInt();
-            head=LinkedList.deleteNodeK(head,nodeVal);
+            head=MyLinkedList.deleteNodeK(head,nodeVal);
             System.out.println("Node With Value "+nodeVal+" is removed:)");
             break;
             default:
@@ -100,31 +100,31 @@ public class Main{
                 case 1:
                 System.out.println("Enter the Value to be Pushed into the Stack");
                 int val=sc.nextInt();
-                stackHead=Stack.addNode(val);
+                stackHead=MyStack.addNode(val);
                 System.out.println("Pushed it---");
                 break;
                 case 2:
-                stackHead=Stack.removeNode();
+                stackHead=MyStack.removeNode();
                 break;
                 case 4:
-                System.out.println("Current Stack Size "+Stack.sizeof());
+                System.out.println("Current Stack Size "+MyStack.sizeof());
                 break;
                 case 3:
-                if(Stack.isEmpty()){
+                if(MyStack.isEmpty()){
                     System.out.println("Oh the Stack is Empty Push Elements");
                 }
-                else{System.out.println("No The Stack's Not Empty there are "+Stack.sizeof()+" Elements in the Stack");}
+                else{System.out.println("No The Stack's Not Empty there are "+MyStack.sizeof()+" Elements in the Stack");}
                 break;
                 case 5:
-                if(Stack.stackTop()==0){
+                if(MyStack.stackTop()==0){
                     System.out.println("The Stack's Empty So There's No Top!");
                 }
                 else{
-                System.out.println("The Top Element in the Stack is "+Stack.stackTop());
+                System.out.println("The Top Element in the Stack is "+MyStack.stackTop());
                 }
                 break;
                 case 6:
-                Stack.printStack(stackHead);
+                MyStack.printStack(stackHead);
                 System.out.println("No Surprise As the Stack Follows ---LIFO---");
                 break;
                 case 7:
@@ -152,43 +152,43 @@ public class Main{
                 case 1:
                 System.out.println("Enter the Value to be Pushed into the Queue");
                 int val=sc.nextInt();
-                queueFront=Queue.push(val);
+                queueFront=MyQueue.push(val);
                 System.out.println("Pushed it :)");
                 break;
                 case 2:
-                if(!Queue.isQEmpty()){
+                if(!MyQueue.isQEmpty()){
                 System.out.println("Removed ;)");
-                queueFront=Queue.pop();
+                queueFront=MyQueue.pop();
                 }
-                else{queueFront=Queue.pop();}
+                else{queueFront=MyQueue.pop();}
                 break;
                 case 4:
-                System.out.println("Current Stack Size "+Queue.sizeofQ());
+                System.out.println("Current Stack Size "+MyQueue.sizeofQ());
                 break;
                 case 3:
-                if(Queue.isQEmpty()){
+                if(MyQueue.isQEmpty()){
                     System.out.println("Oh the Queue is Empty Push Elements");
                 }
-                else{System.out.println("No The Queue's Not Empty there are "+Queue.sizeofQ()+" Elements in the Queue");}
+                else{System.out.println("No The Queue's Not Empty there are "+MyQueue.sizeofQ()+" Elements in the Queue");}
                 break;
                 case 5:
-                if(Queue.peekOfQ()==0){
+                if(MyQueue.peekOfQ()==0){
                     System.out.println("The Queue's Empty So There's No Peek or Last!");
                 }
                 else{
-                System.out.println("The Peek Element in the Queue is "+Queue.peekOfQ());
+                System.out.println("The Peek Element in the Queue is "+MyQueue.peekOfQ());
                 }
                 break;
                 case 6:
-                if(Queue.lastOfQ()==0){
+                if(MyQueue.lastOfQ()==0){
                     System.out.println("The Queue's Empty So There's No Last or Peek!");
                 }
                 else{
-                System.out.println("The Last Element in the Queue is "+Queue.lastOfQ());
+                System.out.println("The Last Element in the Queue is "+MyQueue.lastOfQ());
                 }
                 break;
                 case 7:
-                Queue.printQueue(queueFront);
+                MyQueue.printQueue(queueFront);
                 System.out.println("Queue Follows ---FIFO---");
                 break;
                 case 8:
@@ -198,16 +198,59 @@ public class Main{
             }
         }
       }
-    public static void main(String[] args){
+      public static void handleTrees(Scanner sc){
+            System.out.println("---Welcome to Tree Operations---\n");
+            System.out.println("1. Create a Binary Tree");
+            System.out.println("2. Inorder");
+            System.out.println("3. Preorder");
+            System.out.println("4. PostOrder");
+            System.out.println("5. Height");
+            System.out.println("6. Search");
+            System.out.println("7. Display");
+            System.out.println("8. Back to Main Menu");
+            while(true){
+                int op=sc.nextInt();
+            switch(op){
+                case 1:
+                System.out.println("Enter the nodes level wise Enter -1 for null");
+                List<Integer> nodes=Tree.inputNodeValues(sc);
+                Tree root=Tree.buildTreeFromLevels(nodes);
+                System.out.println("Tree SuccessFully Builttt :)");
+                break;
+                case 2:
+                break;
+                case 4:
+                
+                break;
+                case 3:
+                
+                break;
+                case 5:
+                
+                break;
+                case 6:
+                
+                break;
+                case 7:
+                
+                break;
+                case 8:
+                return;
+                default:
+                System.out.println("Range of Input Integer's Provided For Operation is [1-8]");
+            }
+            }
+      }
+          public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        LinkedList head=null;
+        MyLinkedList head=null;
         System.out.println("Welcome to DSA Simulator");
         System.out.println("Choose a Data Structure");
         while(true){    
             System.out.println("1. Linked Lists");
             System.out.println("2. Stacks");
             System.out.println("3. Queues");
-            System.out.println("4. Trees(Coming soon)");
+            System.out.println("4. Binary Trees(Coming soon)");
             System.out.println("5. Graphs(Coming soon)");
             System.out.println("6. Exit");
             int choice=sc.nextInt();
@@ -222,7 +265,7 @@ public class Main{
                 handleQueue(sc);
                 break;
                 case 4:
-                System.out.println("Coming Soon....");
+                handleTrees(sc);
                 break;
                 case 5:
                 System.out.println("Coming Soon....");
