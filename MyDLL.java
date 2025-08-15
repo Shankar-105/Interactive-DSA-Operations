cf104583a915450f0d8eb8932ba318a41921e9a3
Shankar-105 <shankarbhavani862@gmail.com>
2025-08-15 21:39:14 +0530

    forgot to stage the files in the previous commit now staged the files

--- MyDLL.java
+++ MyDLL.java
@@ -1,3 +1,15 @@
 public class MyDLL {
-    
+    int data;
+    MyDLL next;
+    MyDLL prev;
+    public MyDLL(int val){
+        this.data=val;
+        this.next=null;
+        this.prev=null;
+    }
+    public MyDLL(int val,MyDLL next,MyDLL prev){
+        this.data=val;
+        this.next=next;
+        this.prev=prev;
+    }
 }
